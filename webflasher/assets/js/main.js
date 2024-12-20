bb38630abae8c149379cc60d5f9680c080e664cf
Saurabh Datta <hi@dattasaurabh.com>
2024-12-20 15:14:17 +0100

    startikng to add webflasher

---
+++ webflasher/assets/js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.querySelector('esp-web-install-button');
+    // button.manifest = `manifest.json`;
+  
+    // For GitHub Pages, use the full path to your manifest
+    button.manifest = `${window.location.origin}/help-button-firmware-flasher/manifest.json`;
+    // button.manifest = `${window.location.origin}/help-button-firmware/manifest.json`;
+  });
