ce60e13d98b121715672c0581c830886288b997d
Saurabh Datta <hi@dattasaurabh.com>
2024-12-20 16:58:57 +0100

    corrected the file name

--- webflasher/assets/js/main.js
+++ webflasher/assets/js/main.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const button = document.querySelector('esp-web-install-button');
     // button.manifest = `manifest.json`;
-  
+
     // For GitHub Pages, use the full path to your manifest
     button.manifest = `${window.location.origin}/help-button-firmware/manifest.json`;
   });
